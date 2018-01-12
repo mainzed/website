3d3a7a57bd57d6337043c7583aa38219cce654ca
Axel Kunz <axel.kunz@rocketmail.com>
2018-01-12 16:16:41 +0100

    fixed bug

--- php/common.php
+++ php/common.php
@@ -5,28 +5,31 @@ if ($lang != "") {
 } else {
   $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 }
+
 switch ($lang) {
   case 'en':
   $lang_file = 'lang.en.php';
   break;
+  
   case 'de':
   $lang_file = 'lang.de.php';
   break;
+ 
   default:
   $lang_file = 'lang.de.php';
 }
 
-$isRoot = $_SERVER['REQUEST_URI']=="/";
-$isEvent = $_SERVER['REQUEST_URI']=="/event";
+
+$isRoot = $_SERVER['REQUEST_URI'] == "/";
+$isEvent = $_SERVER['REQUEST_URI'] == "/event";
+
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 
 if ($isRoot) {
-  $host = $_SERVER['HTTP_HOST'];
-  $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
   header("Location: http://$host$uri/$lang");
 } else if ($isEvent) {
-  header("Location: http://localhost:8000/event.php");
+  header("Location: http://$host$uri/event.php");
 } else {
   include_once '../php/'.$lang_file;
 }
-
-?>
